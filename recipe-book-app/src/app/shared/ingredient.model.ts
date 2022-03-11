385a872a49effc354648877c1ee6af3ade6c0712
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-03-11 19:01:14 +0100

    Created shopping list and shopping edit structure

---
+++ recipe-book-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+// export class Ingredient {
+//    public name: string;
+//    public amount: number;
+
+//    constructor(name: string, amonth: number) {
+//       this.name = name;
+//       this.amount = amonth
+//    }
+// }
+
+export class Ingredient {
+   constructor(public name: string, public amount: number) { }
+}
