0bcd6769fcaa09c126d180276a8863b15d82a059
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-03-11 18:18:25 +0100

    Created navigation and recipe structure

--- recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn2.iconfinder.com/data/icons/picnic-filledoutline/64/FOOD_RECIPE-recipe-ingredients-ingredient-education-recipes-orange-books-cooking-512.png'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn2.iconfinder.com/data/icons/picnic-filledoutline/64/FOOD_RECIPE-recipe-ingredients-ingredient-education-recipes-orange-books-cooking-512.png'),
+  ];
 
   constructor() { }
 
