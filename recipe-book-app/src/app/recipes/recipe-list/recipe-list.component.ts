8f3f26ab7e9fbf31305ae16dcf6ad7026c3c0127
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-03-13 11:25:15 +0100

    Adding navigation and adding ingredients
    Adding navigation with event binding and ngIf
    Allowing the user to add ingredients to the shopping list

--- recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn2.iconfinder.com/data/icons/picnic-filledoutline/64/FOOD_RECIPE-recipe-ingredients-ingredient-education-recipes-orange-books-cooking-512.png'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn2.iconfinder.com/data/icons/picnic-filledoutline/64/FOOD_RECIPE-recipe-ingredients-ingredient-education-recipes-orange-books-cooking-512.png'),
+    new Recipe('B Test Recipe', 'This is simply a test', 'https://cdn2.iconfinder.com/data/icons/picnic-filledoutline/64/FOOD_RECIPE-recipe-ingredients-ingredient-education-recipes-orange-books-cooking-512.png'),
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
